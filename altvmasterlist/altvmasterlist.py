788a1d3b90b3915dbe2f1f44943ab50f53c28edf
Nickwasused <34883496+Nickwasused@users.noreply.github.com>
2021-12-30 12:51:17 +0100

    handle offline Server in get_server_by_id

--- altvmasterlist/altvmasterlist.py
+++ altvmasterlist/altvmasterlist.py
@@ -89,7 +89,10 @@ def get_servers():
 
 def get_server_by_id(id):
     temp_data = request(config.server_link.format(id))
-    return_server = Server(temp_data["active"], temp_data["info"]["id"], temp_data["info"]["maxPlayers"], temp_data["info"]["players"], temp_data["info"]["name"], temp_data["info"]["locked"], temp_data["info"]["host"], temp_data["info"]["port"], temp_data["info"]["gameMode"], temp_data["info"]["website"], temp_data["info"]["language"], temp_data["info"]["description"], temp_data["info"]["verified"], temp_data["info"]["promoted"], temp_data["info"]["useEarlyAuth"], temp_data["info"]["earlyAuthUrl"], temp_data["info"]["useCdn"], temp_data["info"]["cdnUrl"], temp_data["info"]["useVoiceChat"], temp_data["info"]["tags"], temp_data["info"]["bannerUrl"], temp_data["info"]["branch"], temp_data["info"]["build"], temp_data["info"]["version"], temp_data["info"]["lastUpdate"])
+    if (temp_data["active"] == False):
+        return_server = Server(temp_data["active"], id, 0, 0, "", False, "0.0.0.0", 7788, "", "", "", "", False, False, False, "", False, "", False, [], "", "release", -1, 0.0, 0)
+    else:
+        return_server = Server(temp_data["active"], id, temp_data["info"]["maxPlayers"], temp_data["info"]["players"], temp_data["info"]["name"], temp_data["info"]["locked"], temp_data["info"]["host"], temp_data["info"]["port"], temp_data["info"]["gameMode"], temp_data["info"]["website"], temp_data["info"]["language"], temp_data["info"]["description"], temp_data["info"]["verified"], temp_data["info"]["promoted"], temp_data["info"]["useEarlyAuth"], temp_data["info"]["earlyAuthUrl"], temp_data["info"]["useCdn"], temp_data["info"]["cdnUrl"], temp_data["info"]["useVoiceChat"], temp_data["info"]["tags"], temp_data["info"]["bannerUrl"], temp_data["info"]["branch"], temp_data["info"]["build"], temp_data["info"]["version"], temp_data["info"]["lastUpdate"])
     return return_server
 
 def get_server_by_id_avg(id, time):
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name='altvmasterlist',
     packages=['altvmasterlist'],
-    version='0.7',
+    version='0.8',
     author="Nickwasused",
     author_email="nickwasused.social@protonmail.com",
     description="A package to use the alt:V Masterlist api",
