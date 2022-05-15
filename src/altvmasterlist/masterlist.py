49b7bde1b256cba5aa8ae5bec0153cf9879ecb9e
Nickwasused <nickwasused.services@protonmail.com>
2022-05-15 12:59:25 +0200

    change string format

--- src/altvmasterlist/masterlist.py
+++ src/altvmasterlist/masterlist.py
@@ -6,11 +6,11 @@
 # Here can you configure stuff like the api endpoint or the url format
 class config:
     base_link = "https://api.altv.mp"
-    all_server_stats_link = "{}/servers".format(base_link)
-    all_servers_link = "{}/servers/list".format(base_link)
-    server_link = "{}/server".format(base_link) + "/{}"
-    server_average_link = "{}/avg".format(base_link) + "/{}/{}"
-    server_max_link = "{}/max".format(base_link) + "/{}/{}"
+    all_server_stats_link = f"{base_link}/servers"
+    all_servers_link = f"{base_link}/servers/list"
+    server_link = f"{base_link}/server" + "/{}"
+    server_average_link = f"{base_link}/avg" + "/{}/{}"
+    server_max_link = f"{base_link}/max" + "/{}/{}"
 
 # This is the server object 
 class Server:
